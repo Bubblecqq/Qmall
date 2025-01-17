75942bb289ff86e74020fa088f340ce2e95ad8a5
cqq <1342982166@qq.com>
2025-01-17 17:20:46 +0800

    add marketing [2]

--- marketing/cmd/api/desc/marketing/internal/svc/serviceContext.go
+++ marketing/cmd/api/desc/marketing/internal/svc/serviceContext.go
@@ -2,14 +2,30 @@ package svc
 
 import (
 	"QMall/marketing/cmd/api/desc/marketing/internal/config"
+	"QMall/marketing/cmd/rpc/activity"
+	"QMall/product/cmd/rpc/product/product"
+	"QMall/user/cmd/rpc/user"
+	"github.com/zeromicro/go-zero/zrpc"
 )
 
 type ServiceContext struct {
-	Config config.Config
+	Config          config.Config
+	ActivityRpcConf activity.ActivityZrpcClient
+	ProductRPC      product.ProductZrpcClient
+	UserRPC         user.UserZrpcClient
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
+	productTarget, _ := zrpc.NewClientWithTarget(c.RemoteCall.ProductRPC)
+	productClient := product.NewProductZrpcClient(productTarget)
+
+	userTarget, _ := zrpc.NewClientWithTarget(c.RemoteCall.UserRPC)
+	userClient := user.NewUserZrpcClient(userTarget)
+
 	return &ServiceContext{
-		Config: c,
+		Config:          c,
+		ProductRPC:      productClient,
+		UserRPC:         userClient,
+		ActivityRpcConf: activity.NewActivityZrpcClient(zrpc.MustNewClient(c.ActivityRpcConf)),
 	}
 }
--- marketing/cmd/rpc/internal/config/config.go
+++ marketing/cmd/rpc/internal/config/config.go
@@ -1,7 +1,26 @@
 package config
 
-import "github.com/zeromicro/go-zero/zrpc"
+import (
+	"github.com/zeromicro/go-zero/core/stores/cache"
+	"github.com/zeromicro/go-zero/zrpc"
+	"github.com/zeromicro/zero-contrib/zrpc/registry/consul"
+)
 
 type Config struct {
 	zrpc.RpcServerConf
+	Mysql      Mysql
+	Cache      cache.CacheConf
+	Consul     consul.Conf
+	RemoteCall RemoteCall
+}
+type Mysql struct {
+	Host     string `json:"host"`
+	User     string `json:"user"`
+	Pass     string `json:"pass"`
+	Database string `json:"database"`
+	Charset  string `json:"charset"`
+}
+type RemoteCall struct {
+	UserRPC    string `json:"UserRPC"`
+	ProductRPC string `json:"ProductRPC"`
 }
